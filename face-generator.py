c34fc15ce1e69b12c6ed3ef38a8b4d771409a41d
number000000 <maggieliulmh@gmail.com>
2022-10-28 01:45:24 -0700

    add customized face components

--- face-generator.py
+++ face-generator.py
@@ -41,17 +41,25 @@ draw()
 
 
 # load face components
-cat_1 = pygame.image.load(os.path.join('data', 'cat_1.png'))
-cat_2 = pygame.image.load(os.path.join('data', 'cat_2.png'))
-cat_3 = pygame.image.load(os.path.join('data', 'cat_3.png'))
-cat_4 = pygame.image.load(os.path.join('data', 'cat_4.png'))
-cat_5 = pygame.image.load(os.path.join('data', 'cat_5.png'))
-cat_6 = pygame.image.load(os.path.join('data', 'cat_6.png'))
-cat_images = [cat_1, cat_2, cat_3, cat_4, cat_5, cat_6]
+eye1 = pygame.image.load(os.path.join('data', 'eye1.png'))
+eye2 = pygame.image.load(os.path.join('data', 'eye2.png'))
+eye3 = pygame.image.load(os.path.join('data', 'eye3.png'))
+eye4 = pygame.image.load(os.path.join('data', 'eye4.png'))
+eyes = [eye1, eye2, eye3, eye4]
+
+mouth1 = pygame.image.load(os.path.join('data', 'mouth1.png'))
+mouth2 = pygame.image.load(os.path.join('data', 'mouth2.png'))
+mouth3 = pygame.image.load(os.path.join('data', 'mouth3.png'))
+mouth4 = pygame.image.load(os.path.join('data', 'mouth4.png'))
+mouths = [mouth1, mouth2, mouth3, mouth4]
 
 
 def draw_left_eye():
-    left_eye = random.choice(cat_images)
+    left_eye = random.choice(eyes)
+    DEFAULT_IMAGE_SIZE = (250, 250)
+    # Scale the image to your needed size
+    left_eye = pygame.transform.scale(left_eye, DEFAULT_IMAGE_SIZE)
+
     img_width = left_eye.get_width()
     img_height = left_eye.get_height()
     x = center_x - (radius / 2) - (img_width / 2)
@@ -63,7 +71,11 @@ draw_left_eye()
 
 
 def draw_right_eye():
-    right_eye = random.choice(cat_images)
+    right_eye = random.choice(eyes)
+    DEFAULT_IMAGE_SIZE = (250, 250)
+    # Scale the image to your needed size
+    right_eye = pygame.transform.scale(right_eye, DEFAULT_IMAGE_SIZE)
+
     img_width = right_eye.get_width()
     img_height = right_eye.get_height()
     x = center_x + (radius / 2) - (img_width / 2)
@@ -74,7 +86,11 @@ def draw_right_eye():
 draw_right_eye()
 
 def draw_mouth():
-    mouth = random.choice(cat_images)
+    mouth = random.choice(mouths)
+    DEFAULT_IMAGE_SIZE = (250, 250)
+    # Scale the image to your needed size
+    mouth = pygame.transform.scale(mouth, DEFAULT_IMAGE_SIZE)
+
     img_width = mouth.get_width()
     img_height = mouth.get_height()
     x = center_x - (img_width / 2)
