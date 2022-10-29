22243df9e2ae14c53302d15e4ddd636aee8c5a84
number000000 <maggieliulmh@gmail.com>
2022-10-28 23:20:22 -0700

    now it can play face garnish; error handling

--- face-generator.py
+++ face-generator.py
@@ -13,13 +13,26 @@ import os.path
 # Initializing pygame
 pygame.init()
 
+# Define a function for exiting program
+def exit_program():
+    pygame.display.quit()
+    pygame.quit()
+    exit()
+
 # Setting up the screen
-screen_wide = (int)(input("The width of the canvas(in pixels): "))
-screen_high = (int)(input("The height of the canvas(in pixels): "))
-rm_bg = (int)(input("Do you want to make the background color transparent?(y=1/n=0): "))
+try:
+    screen_wide = (int)(input("The width of the canvas(in pixels): "))
+    screen_high = (int)(input("The height of the canvas(in pixels): "))
+    rm_bg = (int)(input("Do you want to make the background color transparent?(y=1/n=0): "))
+except ValueError:
+    print("invalid input")
+    exit_program()
 screen = pygame.display.set_mode((screen_wide, screen_high), pygame.SRCALPHA)
 pygame.display.set_caption('Emoji Generator')
 screen = pygame.display.get_surface()
+if rm_bg != 0 or rm_bg != 1:
+    print("invalid input: should be either 0 or 1")
+    exit_program()
 if rm_bg:
     used_color = [9, 255, 0]
 else: 
@@ -33,7 +46,7 @@ if radius * 2 > screen_high:
 center_x = screen_wide / 2
 center_y = screen_high / 2
 
-IMAGE_SIZE = (radius, radius)
+IMAGE_SIZE = (radius, radius) # a variable used to scale the image
 
 # A function for generate the color of the face randomly
 def draw():
@@ -64,7 +77,14 @@ eye6 = pygame.image.load(os.path.join('data', 'eye6.png'))
 eye7 = pygame.image.load(os.path.join('data', 'eye7.png'))
 eye8 = pygame.image.load(os.path.join('data', 'eye8.png'))
 eye9 = pygame.image.load(os.path.join('data', 'eye9.png'))
-eyes = [eye1, eye2, eye3, eye4, eye5, eye6, eye7, eye8]
+eye10 = pygame.image.load(os.path.join('data', 'eye10.png'))
+eye11 = pygame.image.load(os.path.join('data', 'eye11.png'))
+eye12 = pygame.image.load(os.path.join('data', 'eye12.png'))
+eye13 = pygame.image.load(os.path.join('data', 'eye13.png'))
+eye14 = pygame.image.load(os.path.join('data', 'eye14.png'))
+eye15 = pygame.image.load(os.path.join('data', 'eye15.png'))
+eyes_left = [eye1, eye2, eye3, eye4, eye5, eye6, eye7, eye8, eye10, eye12, eye13, eye15]
+eyes_right = [eye1, eye2, eye3, eye4, eye5, eye7, eye8, eye9, eye10, eye11, eye13, eye14]
 
 mouth1 = pygame.image.load(os.path.join('data', 'mouth1.png'))
 mouth2 = pygame.image.load(os.path.join('data', 'mouth2.png'))
@@ -73,11 +93,26 @@ mouth4 = pygame.image.load(os.path.join('data', 'mouth4.png'))
 mouth5 = pygame.image.load(os.path.join('data', 'mouth5.png'))
 mouth6 = pygame.image.load(os.path.join('data', 'mouth6.png'))
 mouth7 = pygame.image.load(os.path.join('data', 'mouth7.png'))
-mouths = [mouth1, mouth2, mouth3, mouth4, mouth5, mouth6, mouth7]
+mouth8 = pygame.image.load(os.path.join('data', 'mouth8.png'))
+mouth9 = pygame.image.load(os.path.join('data', 'mouth9.png'))
+mouth10 = pygame.image.load(os.path.join('data', 'mouth10.png'))
+mouth11 = pygame.image.load(os.path.join('data', 'mouth11.png'))
+mouth12 = pygame.image.load(os.path.join('data', 'mouth12.png'))
+mouth13 = pygame.image.load(os.path.join('data', 'mouth13.png'))
+mouths = [mouth1, mouth2, mouth3, mouth4, mouth5, mouth6, mouth7, mouth8, mouth9, mouth10, mouth11, mouth12, mouth13]
 
+angry = pygame.image.load(os.path.join('data/garnish', 'angry.png'))
+bling = pygame.image.load(os.path.join('data/garnish', 'bling.png'))
+lotsoflove = pygame.image.load(os.path.join('data/garnish', 'lovess.png'))
+exclamation = pygame.image.load(os.path.join('data/garnish', 'exclamation.png'))
+love = pygame.image.load(os.path.join('data/garnish', 'love.png'))
+out_of_words = pygame.image.load(os.path.join('data/garnish', 'out-of-words.png'))
+sleepyy = pygame.image.load(os.path.join('data/garnish', 'sleepy.png'))
+sweat = pygame.image.load(os.path.join('data/garnish', 'sweat.png'))
+garnishes = [angry, bling, lotsoflove, exclamation, love, out_of_words, sleepyy, sweat]
 
 def draw_left_eye():
-    left_eye = random.choice(eyes)
+    left_eye = random.choice(eyes_left)
     # Scale the image to your needed size
     left_eye = pygame.transform.scale(left_eye, IMAGE_SIZE)
     # Position the eye
@@ -88,7 +123,7 @@ def draw_left_eye():
     screen.blit(left_eye, (x, y))
 
 def draw_right_eye():
-    right_eye = random.choice(eyes)
+    right_eye = random.choice(eyes_right)
     # Scale the image to your needed size
     right_eye = pygame.transform.scale(right_eye, IMAGE_SIZE)
     # Position the eye
@@ -109,20 +144,28 @@ def draw_mouth():
     y = center_y + (radius / 2) - (img_height / 2)
     screen.blit(mouth, (x, y))
 
+def draw_garnish():
+    garnish = random.choice(garnishes)
+    # Scale the image to your needed size
+    garnish = pygame.transform.scale(garnish, IMAGE_SIZE)
+    # Position the garnish
+    img_width = garnish.get_width()
+    img_height = garnish.get_height()
+    x = center_x + (3 * radius / 4) - (img_width / 2)
+    y = center_y - (3 * radius / 4) - (img_height / 2)
+    screen.blit(garnish, (x, y))
+
+
 # A wrapper function that call sub-functions to draw eyes and mouth(or no mouth)
 def face_draw():
-    if random.randint(0,1):
-        draw_mouth()
     draw_left_eye()
     draw_right_eye()
+    if random.randint(0,1):
+        draw_mouth()
+    if random.randint(2,3):
+        draw_garnish()
 
 face_draw()
-
-# Define a function for exiting program
-def exit_program():
-    pygame.display.quit()
-    pygame.quit()
-    exit()
 
 # Update the display
 pygame.display.update()
