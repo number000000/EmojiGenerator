9cf62d79fca22e10ad235f52a7f009bc13d84039
number000000 <maggieliulmh@gmail.com>
2022-10-28 23:20:55 -0700

    add correct file naming logic

--- background_remove.py
+++ background_remove.py
@@ -8,6 +8,7 @@ bg_color - the rgb value of the background color
 Code modified from https://stackoverflow.com/questions/765736/how-to-use-pil-to-make-all-white-pixels-transparent
 """
 from PIL import Image
+import os
 
 def remove_background(filepath, bg_color):
     img = Image.open(filepath)
@@ -22,4 +23,16 @@ def remove_background(filepath, bg_color):
             newData.append(item)
 
     img.putdata(newData)
-    img.save("face_nopg.png", "PNG")
+    img.save("tmp.png", "PNG")
+
+    #rename the image
+    m = 1
+    while True:
+        fileName = "face_nobg{}.png"
+        fileName = fileName.format(m)
+        if not os.path.exists(fileName):
+            fileName = "face_nobg{}.png"
+            fileName = fileName.format(m)
+            os.rename("tmp.png", fileName)
+            break
+        m+=1
